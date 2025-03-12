17fcdd6dfe2e4c6e6b75f58aa01a741189f091bd
chiragdhunna <chiragdhunna2468@gmail.com>
2025-03-12 22:04:05 +0530

    initial setup

---
+++ database/db.js
@@ -0,0 +1,23 @@
+var mysql = require("mysql2");
+var dotenv = require("dotenv");
+
+dotenv.config({
+  path: "../.env",
+});
+
+// Create a connection
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  // password: "5wj9u#nJ",
+});
+
+// Connect to MySQL
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    return;
+  }
+  console.log("Connected to MySQL database!");
+});

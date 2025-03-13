e48e29298cdf075a85b936be21764c2b832da12b
chiragdhunna <chiragdhunna2468@gmail.com>
2025-03-13 10:27:33 +0530

    mysql setup + package.json update for modules

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const express = require("express");
+import "dotenv/config";
+
+import express from "express";
+import connection from "./database/db.js";
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  connection.query("show databases", (err, results) => {
+    if (err) {
+      res.send("Error : " + err);
+    } else {
+      res.json({ databases: results });
+    }
+  });
 });
 
 app.listen(port, () => {
--- database/db.js
+++ database/db.js
@@ -1,15 +1,11 @@
-var mysql = require("mysql2");
-var dotenv = require("dotenv");
-
-dotenv.config({
-  path: "../.env",
-});
+import mysql from "mysql2";
 
 // Create a connection
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 // Connect to MySQL
@@ -18,5 +14,8 @@ connection.connect((err) => {
     console.error("Error connecting to MySQL:", err);
     return;
   }
+
   console.log("Connected to MySQL database!");
 });
+
+export default connection;
